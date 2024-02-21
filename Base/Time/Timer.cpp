596ced3cf54fe1c108c1e99e8e21476edfa5aafb
Taganyer <709349067@qq.com>
2024-02-21 10:54:06 +0800

    Time-Timer push

---
+++ Base/Time/Timer.cpp
@@ -0,0 +1,97 @@
+//
+// Created by taganyer on 24-2-13.
+//
+
+#include "Timer.hpp"
+#include <sys/timerfd.h>
+#include <unistd.h>
+
+#include "../Exception.hpp"
+
+namespace Base {
+
+    Timer::~Timer() { close_fd(); }
+
+    void Timer::invoke(int32 timeouts) {
+        if (timeouts == 0 || _time.nanoseconds <= 0) return;
+        start();
+        if (timeouts < 0) {
+            while (true) {
+                if (uint64 expirations; read(fd, &expirations, sizeof(expirations)) == -1)
+                    throw Exception("Timer::invoke() running failed");
+                if (_fun) _fun();
+            }
+        } else {
+            while (timeouts) {
+                if (uint64 expirations; read(fd, &expirations, sizeof(expirations)) == -1)
+                    throw Exception("Timer::invoke() running failed");
+                if (_fun) _fun();
+                --timeouts;
+            }
+        }
+        stop();
+    }
+
+    void Timer::sleep(int32 timeouts) {
+        if (timeouts <= 0 || _time.nanoseconds <= 0) return;
+        struct timespec time{};
+        time.tv_sec = _time.nanoseconds / SEC_ * timeouts;
+        time.tv_nsec = _time.nanoseconds % SEC_;
+        time.tv_sec += timeouts * time.tv_nsec / SEC_;
+        time.tv_nsec += timeouts * time.tv_nsec % SEC_;
+
+        nanosleep(&time, nullptr);
+    }
+
+    void Timer::start() {
+        struct itimerspec timeout{};
+        int64 sec = _time.nanoseconds / SEC_;
+        int64 ns = _time.nanoseconds % SEC_;
+        timeout.it_value.tv_sec = timeout.it_interval.tv_sec = sec;
+        timeout.it_value.tv_nsec = timeout.it_interval.tv_nsec = ns;
+
+        if (fd == -1) {
+            fd = timerfd_create(CLOCK_REALTIME, 0);
+            if (fd == -1) throw Exception("Timer::start() fd create failed");
+        }
+        if (timerfd_settime(fd, 0, &timeout, nullptr) == -1)
+            throw Exception("Timer::start() set failed");
+    }
+
+    void Timer::stop() {
+        struct itimerspec zero = {};
+        zero.it_interval.tv_sec = zero.it_value.tv_sec = 0;
+        zero.it_interval.tv_nsec = zero.it_value.tv_nsec = 0;
+
+        if (timerfd_settime(fd, 0, &zero, nullptr) == -1)
+            throw Exception("Timer::stop() fail\n");
+    }
+
+    int32 Timer::release_fd() {
+        start();
+        int32 temp = fd;
+        fd = -1;
+        return temp;
+    }
+
+    void Timer::close_fd() {
+        if (fd != -1) close(fd);
+    }
+
+    Time_difference Unix_to_now() {
+        struct timespec now{};
+        clock_gettime(CLOCK_REALTIME, &now);
+        int64 ns = SEC_;
+        ns *= now.tv_sec;
+        ns += now.tv_nsec;
+        return {ns};
+    }
+
+    void sleep(Time_difference time) {
+        struct timespec timespec{};
+        timespec.tv_sec = time.nanoseconds / SEC_;
+        timespec.tv_nsec = time.nanoseconds % SEC_;
+        nanosleep(&timespec, nullptr);
+    }
+
+}
---
+++ Base/Time/Timer.hpp
@@ -0,0 +1,138 @@
+//
+// Created by taganyer on 24-2-13.
+//
+
+#ifndef TEST_TIMER_HPP
+#define TEST_TIMER_HPP
+
+#include <ctime>
+#include <functional>
+#include "../Detail/config.hpp"
+
+namespace Base {
+
+    constexpr int64 US_ = 1000;
+    constexpr int64 MS_ = 1000000;
+    constexpr int64 SEC_ = 1000000000;
+    constexpr int64 MIN_ = 60 * SEC_;
+    constexpr int64 HOUR_ = 60 * MIN_;
+
+    struct Time_difference {
+
+        int64 nanoseconds = 0;
+
+        Time_difference() = default;
+
+        Time_difference(int64 ns) : nanoseconds(ns) {};
+
+        operator int64() const { return nanoseconds; };
+
+        [[nodiscard]] double to_hour() const { return (double) nanoseconds / HOUR_; };
+
+        [[nodiscard]] double to_min() const { return (double) nanoseconds / MIN_; };
+
+        [[nodiscard]] double to_sec() const { return (double) nanoseconds / SEC_; };
+
+        [[nodiscard]] double to_ms() const { return (double) nanoseconds / MS_; };
+
+        [[nodiscard]] double to_us() const { return (double) nanoseconds / US_; };
+
+    };
+
+    class Timer {
+    public:
+
+        using Fun = std::function<void()>;
+
+        Timer() = default;
+
+        Timer(const Timer &other) : _time(other._time), _fun(other._fun) {};
+
+        Timer(int64 ns) : _time(ns) {}
+
+        Timer(int64 ns, Timer::Fun fun) : _time(ns), _fun(std::move(fun)) {};
+
+        ~Timer();
+
+        void invoke(int32 timeouts);
+
+        void sleep(int32 timeouts = 1);
+
+        int32 release_fd();
+
+        void reset_time(int64 ns) { _time.nanoseconds = ns; };
+
+        void reset_fun(Fun fun) { _fun = std::move(fun); };
+
+        void close_fd();
+
+        [[nodiscard]] Time_difference get_timeout() const { return _time; };
+
+        [[nodiscard]] int32 get_fd() const { return fd; };
+
+    private:
+
+        Time_difference _time;
+
+        Fun _fun;
+
+        int32 fd = -1;
+
+        void start();
+
+        void stop();
+
+    };
+
+    Time_difference Unix_to_now();
+
+    void sleep(Time_difference time);
+
+    template<typename Fun, typename ...Args>
+    Time_difference chronograph(Fun &&fun, Args &&...args) {
+        struct timespec startTime{}, endTime{};
+        clock_gettime(CLOCK_REALTIME, &startTime);
+        fun(std::forward<Args>(args)...);
+        clock_gettime(CLOCK_REALTIME, &endTime);
+        int64 ns = SEC_;
+        ns *= (endTime.tv_sec - startTime.tv_sec);
+        ns += (endTime.tv_nsec - startTime.tv_nsec);
+        return {ns};
+    }
+
+    inline int64 operator ""ns(uint64 ns) {
+        return (int64) ns;
+    }
+
+    inline int64 operator ""us(uint64 us) {
+        return (int64) us * US_;
+    }
+
+    inline int64 operator ""ms(uint64 ms) {
+        return (int64) ms * MS_;
+    }
+
+    inline int64 operator ""s(uint64 sec) {
+        return (int64) sec * SEC_;
+    }
+
+    inline int64 operator ""min(uint64 min) {
+        return (int64) min * MIN_;
+    }
+
+    inline int64 operator ""h(uint64 hour) {
+        return (int64) hour * HOUR_;
+    }
+
+    inline Time_difference operator+(const Time_difference &left, const Time_difference &right) {
+        return {left.nanoseconds + right.nanoseconds};
+    }
+
+    inline Time_difference operator-(const Time_difference &left, const Time_difference &right) {
+        return {left.nanoseconds - right.nanoseconds};
+    }
+
+}
+
+
+#endif //TEST_TIMER_HPP
