96ef5511d01f93a00aa7b609882e391888437dd6
Taganyer <709349067@qq.com>
2024-02-21 10:51:31 +0800

    Pool push

---
+++ Base/Detail/Pool.hpp
@@ -0,0 +1,270 @@
+//
+// Created by taganyer on 24-2-6.
+//
+
+#ifndef BASE_POOL_HPP
+#define BASE_POOL_HPP
+
+#include "config.hpp"
+#include "../Thread.hpp"
+#include "../Exception.hpp"
+#include <mutex>
+#include <condition_variable>
+
+namespace Base::Detail {
+
+    struct PoolData {
+        using A_Size = std::atomic<uint32>;
+        using Size = uint32;
+        using State = std::atomic<uint8>;
+        using Mutex = std::mutex;
+        using Condition = std::condition_variable;
+        using Lock = std::unique_lock<Mutex>;
+
+        enum { RUNNING = 0, RESUMING = 2, STOP = 4, SHUTTING = 8, TERMINATED = 16 };
+
+        A_Size core_threads = 0;
+
+        Size waiting_tasks = 0;
+
+        State state = TERMINATED;
+
+        Mutex lock;
+
+        Condition consume, submit;
+    };
+
+
+    template<typename Creator>
+    class Pool {
+    public:
+        using Fun = typename Creator::Fun;
+        using Task = typename Creator::Task;
+        using Size = PoolData::Size;
+        using Lock = PoolData::Lock;
+
+        explicit Pool(Creator &&creator, PoolData *data) :
+                creator(std::move(creator)), data(data) {};
+
+        ~Pool();
+
+        void create_threads(Size size);
+
+        void delete_threads(Size size);
+
+        void submit_to_top(Fun &&fun);
+
+        void submit_to_tail(Fun &&fun);
+
+        void delete_tasks(Size size);
+
+        void stop();
+
+        void start();
+
+        void clear();
+
+        void shutdown();
+
+        [[nodiscard]] Size threads_size() const { return data->core_threads.load(); };
+
+        [[nodiscard]] Size tasks_size() const { return data->waiting_tasks; };
+
+        [[nodiscard]] bool stopping() const { return data->state.load() > Base::Detail::PoolData::RESUMING; };
+
+        [[nodiscard]] bool takeable() const { return head; };
+
+
+    private:
+
+        Creator creator;
+
+        PoolData *data;
+
+        Task *head = nullptr, *tail = nullptr;
+
+        inline Task *get_task();
+
+        inline void end_thread();
+
+        inline void task_to_top(Task *task);
+
+        inline void task_to_tail(Task *task);
+
+    };
+}
+
+namespace Base::Detail {
+
+    template<typename Creator>
+    Pool<Creator>::~Pool() {
+        shutdown();
+        Lock l(data->lock);
+        data->submit.wait(l, [this]() {
+            return data->state.load() == Base::Detail::PoolData::TERMINATED;
+        });
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::create_threads(Pool::Size size) {
+        Size record = data->core_threads.load() + size;
+        for (Size i = 0; i < size; ++i) {
+            Thread thread(string("threadPool"), [this] {
+                Creator item = creator.create_thread();
+                data->core_threads.fetch_add(1);
+                Task *task;
+                while (true) {
+                    {
+                        Lock l(data->lock);
+                        data->submit.notify_one();
+                        item.thread_wait(l, data->consume);
+                        task = get_task();
+                    }
+                    if (item.invoke_task(task))
+                        break;
+                }
+                end_thread();
+            });
+            thread.start();
+        }
+        Lock l(data->lock);
+        data->submit.wait(l, [this, record] { return data->core_threads == record; });
+        if (data->state.load() == Base::Detail::PoolData::TERMINATED)
+            data->state.store(Base::Detail::PoolData::RUNNING);
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::delete_threads(Pool::Size size) {
+        Lock l(data->lock);
+        for (Size i = 0; i < size; ++i) {
+            task_to_top(creator.close_thread());
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::submit_to_top(Pool::Fun &&fun) {
+        auto task = creator.create_task(std::move(fun));
+        if (stopping()) {
+            creator.delete_task(task);
+            return;
+        }
+        Lock l(data->lock);
+        creator.task_wait(l, data->submit);
+        if (stopping()) {
+            creator.delete_task(task);
+        } else {
+            task_to_top(task);
+            data->consume.notify_one();
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::submit_to_tail(Pool::Fun &&fun) {
+        auto task = creator.create_task(std::move(fun));
+        if (stopping()) {
+            creator.delete_task(task);
+            return;
+        }
+        Lock l(data->lock);
+        creator.task_wait(l, data->submit);
+        if (stopping()) {
+            creator.delete_task(task);
+        } else {
+            task_to_tail(task);
+            data->consume.notify_one();
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::delete_tasks(Size size) {
+        Lock l(data->lock);
+        while (size && head) {
+            auto task = get_task();
+            creator.delete_task(task);
+            --size;
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::stop() {
+        if (data->state.load() == Base::Detail::PoolData::RUNNING) {
+            data->state.store(Base::Detail::PoolData::STOP);
+            data->submit.notify_all();
+        }
+    }
+
+
+    template<typename Creator>
+    void Pool<Creator>::start() {
+        if (data->state.load() == Base::Detail::PoolData::STOP) {
+            Lock l(data->lock);
+            data->state.store(Base::Detail::PoolData::RESUMING);
+            task_to_tail(creator.create_task([this](bool) {
+                data->state.store(Base::Detail::PoolData::RUNNING);
+                return false;
+            }));
+            data->consume.notify_all();
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::clear() {
+        Lock l(data->lock);
+        while (head) {
+            auto task = get_task();
+            creator.delete_task(task);
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::shutdown() {
+        Lock l(data->lock);
+        if (data->state.load() > Base::Detail::PoolData::STOP) return;
+        data->state.store(Base::Detail::PoolData::SHUTTING);
+        while (head) {
+            auto task = get_task();
+            creator.delete_task(task);
+        }
+        for (Size i = 0; i < data->core_threads; ++i) {
+            task_to_top(creator.close_thread());
+        }
+        data->consume.notify_all();
+    }
+
+
+    template<typename Creator>
+    typename Pool<Creator>::Task *Pool<Creator>::get_task() {
+        auto temp = head;
+        head = head->next;
+        if (!head) tail = nullptr;
+        --data->waiting_tasks;
+        return temp;
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::end_thread() {
+        if (data->core_threads.fetch_sub(1) == 1) {
+            data->state.store(Base::Detail::PoolData::TERMINATED);
+            data->submit.notify_all();
+        }
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::task_to_top(Pool::Task *task) {
+        if (head) task->next = head;
+        else tail = task;
+        head = task;
+        ++data->waiting_tasks;
+    }
+
+    template<typename Creator>
+    void Pool<Creator>::task_to_tail(Pool::Task *task) {
+        if (tail) tail->next = task;
+        else head = task;
+        tail = task;
+        ++data->waiting_tasks;
+    }
+
+}
+
+#endif //BASE_POOL_HPP
---
+++ Base/Detail/config.hpp
@@ -0,0 +1,86 @@
+//
+// Created by taganyer on 24-2-5.
+//
+
+#ifndef BASE_CONFIG_HPP
+#define BASE_CONFIG_HPP
+
+namespace Base {
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+#define SIZEOF_LONG_ 4
+
+#elif defined(__linux__) || defined(__gnu_linux__) || defined(__APPLE__) || defined(__MACH__)
+#define SIZEOF_LONG_ 8
+
+#else
+#error "Unknown platform"
+#endif
+
+    using sizet = unsigned long long;
+
+    using diff = long long;
+
+    using int8 = char;
+
+    using int16 = short;
+
+    using int32 = int;
+
+    using int64 = long long;
+
+    using uint8 = unsigned char;
+
+    using uint16 = unsigned short;
+
+    using uint32 = unsigned int;
+
+    using uint64 = unsigned long long;
+
+    constexpr short MAX_SHORT = 32767;
+
+    constexpr short MIN_SHORT = -32768;
+
+    constexpr short MAX_USHORT = -1;
+
+    constexpr int MAX_INT = 2147483647;
+
+    constexpr int MIN_INT = -2147483648;
+
+    constexpr unsigned int MAX_UINT = static_cast<const unsigned int>(-1);
+
+#if SIZEOF_LONG_ == 4
+    constexpr long MAX_LONG = 2147483647;
+
+    constexpr long MIN_LONG = -2147483648;
+
+    constexpr unsigned long MAX_ULONG = -1;
+
+#elif SIZEOF_LONG_ == 8
+    constexpr long MAX_LONG = 9223372036854775807;
+
+    constexpr long MIN_LONG = static_cast<const long>(-9223372036854775808);
+
+    constexpr unsigned long MAX_ULONG = static_cast<const unsigned long>(-1);
+
+#else
+#error "Unknown platform"
+#endif
+
+    constexpr long long MAX_LLONG = 9223372036854775807;
+
+    constexpr long long MIN_LLONG = static_cast<const long long int>(-9223372036854775808);
+
+    constexpr unsigned long long MAX_ULLONG = static_cast<const unsigned long long int>(-1);
+
+    constexpr float MAX_FLOAT = 3.402823466e+38f;
+
+    constexpr float MIN_FLOAT = -3.402823466e+38f;
+
+    constexpr double MAX_DOUBLE = 1.7976931348623157e+308;
+
+    constexpr double MIN_DOUBLE = -1.7976931348623157e+308;
+
+}
+
+#endif //BASE_CONFIG_HPP
